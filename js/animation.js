6fe9e65656f7be3f2d7a06e4e33894e8c3babf76
Pavel <IU7.2@yandex.ru>
2018-04-07 23:47:46 +0300

    Комменты

--- js/animation.js
+++ js/animation.js
@@ -3,6 +3,7 @@ var time = performance.now();
 //values to detec direction of scrolling
 var prevScrolltop;
 
+//  Установка связей для плавной анимации переходов
 function bindMenuScroll() {
 	$('#href-cap').click(function () {
 		scroll('#capitan');
@@ -23,6 +24,7 @@ function bindMenuScroll() {
 		scroll('#footer');
 	})
 
+	//  Переход к объекту на странице
 	function scroll(elem) {
 		$('html, body').animate({
 			scrollTop: $(elem).offset().top
@@ -30,11 +32,15 @@ function bindMenuScroll() {
 	}
 }
 
+
+//  Анимация сектора
 function animateSection(id) {
 	const section = $(id);
+	//  Проверка на видимость сектора
 	if (!$(section).isOnScreen(0.3, 0.3))
 		return;
 
+	//  Была ли проиграна анимация
 	if ($(section).attr('animate') == 'true')
 		return;
 
@@ -46,39 +52,50 @@ function animateSection(id) {
 	animateKnowledges();
 	return;
 
+	//  Анимация ФИО
 	function animateInitial() {
 		$(section).find('.section__content_info_card_initial-div').removeClass('hidden');
 		return;
 	}
 
+	//  Анимация анкеты
 	function animateContainer() {
+		//  Найти все текстовые поля
 		let elems = $(section).find('.field');
 		elems.sort((a, b) => {
 			if (Number($(a).attr('num')) > Number($(b).attr('num')))
 				return 1;
 			return -1;
 		});
+
+		//  Установить время задержки анимации
 		for (let I = 0; I < elems.length; I++) {
 			$(elems[I]).css('transition-delay', ((I + 1) * 200)+'ms');
 		}
 
+		//  Удалить класс, который маскирует элементы
 		$(elems).removeClass('hidden').removeClass('rhidden');
 		return;
 	}
 
+	//  Анимирование "знаний"
 	function animateKnowledges() {
+		//  Найти все знания
 		let knowledges = $(section).find(".knowledges_container__knowledge-div");
 		knowledges.sort((a, b) => {
 			if (Number($(a).attr('num')) > Number($(b).attr('num')))
 				return 1;
 			return -1;
 		});
+		//  Установить время задержки анимации
 		for (let I = 0; I < knowledges.length; I++) {
 			$(knowledges[I]).css('transition-delay', ((I + 1) * 200)+'ms');
 		}
+		//  Удалить класс, который маскирует элементы
 		$(knowledges).removeClass('hidden');
 	}
 
+	//  Анимация фотографии 
 	function animatePhoto() {
 		$(section).find('img.photo-img').removeClass('hidden');
 		return;
@@ -86,6 +103,7 @@ function animateSection(id) {
 
 }
 
+//  Инициализация движка анимации
 function engineScrollAnimation() {
 	window.onscroll = function () {
 		var time_delta = (performance.now() - time);
@@ -101,6 +119,7 @@ function engineScrollAnimation() {
 	}
 }
 
+//  Блоки анимации
 function checkAnimationOnScreen() {
 	animateSection('#capitan');
 	animateSection('#pasha');
@@ -110,12 +129,16 @@ function checkAnimationOnScreen() {
 	return;
 }
 
+// Анимация футера
 function animateFooter(){
 	const section = $('#members-container');
 	if (!$(section).isOnScreen(0.2, 0.2))
 		return;
 	
 	animateMember();
+	return;
+
+	//  Анимация участников
 	function animateMember() {
 		let members = $(section).find(".member");
 		members.sort((a, b) => {
@@ -130,9 +153,9 @@ function animateFooter(){
 	}
 }
 
+//  Инициализация
 $(document).ready(function () {
 	bindMenuScroll();
 	checkAnimationOnScreen();
 	engineScrollAnimation();
-
 });
